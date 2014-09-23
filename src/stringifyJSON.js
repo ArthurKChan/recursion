64485309f5756891f9b6b1f4810057b4cb2b440a
Arfurboy <arfurboy@gmail.com>
2014-09-23 16:37:41 -0700

    Completed recursive stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,31 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  var type = typeof(obj) /* number,string, or object */
-
-  switch (type) {
-    case 'number':
-      return stringifyJSON_number(obj);
-      break;
-    case 'string':
-      return stringifyJSON_string(obj);
-      break;
-    case 'object':
-      return stringifyJSON_object(obj);
-      break;
-    default:
-      console.log('did no recognize obj type');
+  var result = '';
+  if (typeof obj !== 'object' || obj === null){
+    /* BASE CASE */
+    return typeof obj === 'string' ? '"'+obj+'"' : String(obj);
   }
-};
+  else {
+    if(obj instanceof Array){
+      result += '[';
+      for(var i=0; i < obj.length; i++){
+        if( obj[i] instanceof Function || obj[i] === undefined){/*array functions and undefined values are set to 'null'*/
+          result += 'null,';
+        }
+        else{
+          /* JSON.stringify(Array) only works on 0-n indexed values, therefore using obj.length*/
+          /* appends result in Array format */
+          result += stringifyJSON(obj[i])+',';
+        }
+      }
+      /* delete hanging ',' */
+      if(obj.length > 0){ result = result.slice(0,result.length-1); }
+      result += ']';
+    }
+    else{
+      result += '{';
+      for(var i in obj){
+        if( !(obj[i] instanceof Function) && obj[i] !== undefined){/*keys with functions and undefined values are ignored*/
 
-var stringifyJSON_object = function(obj){
-  return 'object';
-}
-
-var stringifyJSON_string = function(obj){
-  return 'string';
-}
-
-var stringifyJSON_number = function(obj){
-  return 'number';
+          /* stringifies all keys of the object */
+          /* appends result in Object format */
+          result += '"' + String(i) + '":' + stringifyJSON(obj[i]) + ',';
+        }
+      }
+      if(result[result.length-1] === ','){
+        /* delete hanging ',' */
+        result = result.slice(0,result.length-1);
+      }
+      result += '}';
+    }    
+    return result;
+  }
 }
