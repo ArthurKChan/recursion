c1182d42ae8d3c205a68a0950d9b903e66546723
Arfurboy <arfurboy@gmail.com>
2014-09-24 15:16:40 -0700

    Added tokenize(), takes JSON string and returns array of tokens

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -5,3 +5,48 @@
 var parseJSON = function(json) {
   // your code goes here
 };
+
+/*  Input:  JSON string
+    Output: list of tokens from the string
+*/
+function tokenize(_st){
+  var list = [], st = new Array(), token;
+  st = _st.split('');
+  token = st.shift();
+  /* shift off tokens from st and push them into list */
+
+  while( st.length > 0 ){
+
+    /* check if single char token */
+    if( (/\{|\:|\,|\}|\[|\]|"/).test(token) ){
+      list.push(token);
+      token = st.shift();
+    }
+
+    else if ( (/[0-9]/).test(token) ){
+      /* NUMBER TOKEN */
+      var temp=[];
+      while ( (/[0-9]|\./).test(token) ){
+        temp.push(token);
+        token = st.shift();
+      }
+      list.push(temp.join(''));
+    }
+
+    else if ( (/[a-zA-Z]|_/).test(token) ){
+      /* WORD TOKEN */
+      var temp=[];
+      while ( (/[a-zA-Z0-9]|_/).test(token) ){
+      /* 0-9 can be other chars in word token */
+        temp.push(token);
+        token = st.shift();
+      }
+      list.push(temp.join(''));
+    }
+    else{
+      console.log('Could not recognize token!');
+    }
+  }
+  return list;
+}
+
